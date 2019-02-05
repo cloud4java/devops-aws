ba90b2b075053df9452736e6db59d36e80248dfc
Dorival <dorival.querino@gmail.com>
2019-02-04 22:33:21 -0200

    Added command to run and terminate machine.

--- aws-cmd.cmd
+++ aws-cmd.cmd
@@ -8,3 +8,13 @@ chmod 600 /home/dorival/.ssh/lamp2Key.pem
 aws ec2 run-instances --image-id --instance-type t2-micro --key-name lamp2Key --security-group-ids sg-0a58c5622bcaa02cb
 aws ec2 describe-instance-status --instance-id i-123
 Sessao 3 aula 10
+aws ec2 run-instances --image-id ami-0799ad445b5727125 --instance-type t2.micro --key-name lamp2Key --security-group-ids sg-0a58c5622bcaa02cb
+aws ec2 describe-instance-status --instance-id i-0551450c19accdd6a --output text
+aws ec2 describe-instances  --instance-id i-0551450c19accdd6a --query "Reservations[].Instances[].PublicDnsName"
+
+
+aws ec2 describe-instances --instance-id i-0551450c19accdd6a --output table
+ssh -i /home/dorival/.ssh/lamp2Key.pem ec2-user@ec2-54-183-148-69.us-west-1.compute.amazonaws.com
+chmod 400 /home/dorival/.ssh/lamp2Key.pem
+aws ec2 describe-instances --instance-ids i-0551450c19accdd6a --output text
+aws ec2 terminate-instances --instance-ids i-0551450c19accdd6a
